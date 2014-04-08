c67683231173f2405f1c4d0da484d538b3b5c0d7
Chris Hinds <drudger57@gmail.com>
2014-04-08 04:03:29 -0500

    Added pages controller, update routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def latest
+    @client = $client
+  end
+
+  def reddit
+  end
+
+  def twitter
+  end
+
+  def youtube
+  end
+
+  def about
+  end
+
+  def faq
+  end
+end
