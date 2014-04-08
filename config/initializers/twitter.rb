424e4452f040b8b40c10ea36d2736c995ca25a9c
Chris Hinds <drudger57@gmail.com>
2014-04-08 03:58:32 -0500

    Added twitter gem, configured

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,6 @@
-client = Twitter.REST.Client.new do |config|
-  config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token = ENV['TWITTER_ACCESS_KEY']
+  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
 end
