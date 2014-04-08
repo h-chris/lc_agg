85dddc0fb9c97af49dc404d86cca4221bb1550fb
Chris Hinds <drudger57@gmail.com>
2014-04-08 07:39:41 -0500

    Twitter config now uses vars rather than raw

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,6 @@
-client = Twitter::REST::Client.new do |config|
+require_relative '../twitter_vars'
+
+$client = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
   config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
   config.access_token = ENV['TWITTER_ACCESS_KEY']
