f20a338768c4e0b67cfa1278324be81c822b2989
Chris Hinds <drudger57@gmail.com>
2014-04-08 05:16:45 -0500

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 LcAgg::Application.routes.draw do
   root 'pages#latest'
 
-  match: '/reddit'  to: 'pages#reddit',  via: 'get'
-  match: '/twitter' to: 'pages#twitter', via: 'get'
-  match: '/youtube' to: 'pages#youtube', via: 'get'
-  match: '/about'   to: 'pages#about',   via: 'get'
-  match: '/faq'     to: 'pages#faq',     via: 'get'
+  match '/reddit',  to: 'pages#reddit',  via: 'get'
+  match '/twitter', to: 'pages#twitter', via: 'get'
+  match '/youtube', to: 'pages#youtube', via: 'get'
+  match '/about',   to: 'pages#about',   via: 'get'
+  match '/faq',     to: 'pages#faq',     via: 'get'
 end
