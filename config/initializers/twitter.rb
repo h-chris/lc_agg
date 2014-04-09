2c93430411bec8b28aaa7d44a1bc04d454b1a59c
Chris Hinds <drudger57@gmail.com>
2014-04-08 20:27:32 -0500

    Heroku/Twitter configuration attempt

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,5 +1,3 @@
-require_relative '../twitter_vars'
-
 $client = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
   config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
