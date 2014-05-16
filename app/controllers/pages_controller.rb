2781c1bffa3357dbb021a2a0532e0d5c81915317
Chris Hinds <drudger57@gmail.com>
2014-05-16 02:52:50 -0500

    Cleaned up view/controller testing code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,6 @@ class PagesController < ApplicationController
     Tweet.new.update_t_db
     @reddits = RedditPost.order('posted_at DESC')
     @tweets  = Tweet.order('tweeted_at DESC')
-    @yt = RedditPost.all.where('domain=? OR domain=?', 
-                               'youtu.be', 'youtube.com')
-    @yt_embed = "http://youtube.com/embed/"
-    @yt_short = "http://youtu.be/"
-    @yt_long  = "https://www.youtube.com/watch?v="
   end
 
   def youtube
