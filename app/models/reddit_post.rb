26ab8102fcde96029f5d68eebdec82ac6ad804d7
Chris Hinds <drudger57@gmail.com>
2014-05-16 08:40:18 -0500

    Moved json parse function to a module

--- app/models/reddit_post.rb
+++ app/models/reddit_post.rb
@@ -1,5 +1,8 @@
 class RedditPost < ActiveRecord::Base
   include ActionView::Helpers::DateHelper
+  require 'parse_utils'
+  include ParseUtils
+
   # var names
   # title
   # author
@@ -36,30 +39,6 @@ class RedditPost < ActiveRecord::Base
       obj = data[i]['data']
       insert_listing(obj) unless obj.nil?
     end # end loop
-  end
-
-  def parseJSON(url)
-
-    # url to use
-    uri = URI.parse(url)
-
-    # create http request from url
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri)
-
-    # store response data
-    response = http.request(request)
-
-    # store parsed data if request successful
-    if response.code == "200"
-      result = JSON.parse(response.body)
-    # else set to nil
-    else
-      result = nil
-    end
-
-    # return object
-    return result
   end
 
   def insert_listing(obj)
---
+++ lib/parse_utils.rb
@@ -0,0 +1,25 @@
+module ParseUtils
+  def parseJSON(url)
+
+    # url to use
+    uri = URI.parse(url)
+
+    # create http request from url
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    # store response data
+    response = http.request(request)
+
+    # store parsed data if request successful
+    if response.code == "200"
+      result = JSON.parse(response.body)
+    # else set to nil
+    else
+      result = nil
+    end
+
+    # return object
+    return result
+  end
+end
