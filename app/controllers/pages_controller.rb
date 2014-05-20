9b240506760ba2e3eb4a9ef3ed3352652414cf3f
Chris Hinds <drudger57@gmail.com>
2014-05-20 01:37:25 -0500

    git fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,7 @@ class PagesController < ApplicationController
   def latest
     RedditPost.new.update_r_db
     Tweet.new.update_t_db
-<<<<<<< HEAD
-    @reddits = RedditPost.order('posted_at DESC')
-    @tweets  = Tweet.order('tweeted_at DESC')
-  end
-=======
     Youtube.new.update_yt_db
->>>>>>> add_youtube_videos
 
     @reddits  = RedditPost.order('posted_at DESC')
     @tweets   = Tweet.order('tweeted_at DESC')
